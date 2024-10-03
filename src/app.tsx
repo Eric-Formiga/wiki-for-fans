08039af782a4ae697f692864284bca7c40ea8086
Eric Formiga <eric.13formiga@gmail.com>
2024-10-02 21:54:18 -0300

    feat: Configured  Router for Home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
-
 import './app.css'
+import { BrowserRouter, Route, Routes  } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 export function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>  
     </>
   )
 }
