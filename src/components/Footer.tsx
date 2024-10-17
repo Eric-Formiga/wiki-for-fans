d98178c7f569005a1e771666e1fc95c7d20a515e
Eric-Formiga <eric.13formiga@gmail.com>
2024-10-16 22:15:13 -0300

    feat: add navbar and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-800 p-4 flex justify-around shadow-md">
+      <div className="flex items-center gap-16">
+        <h1 className="text-white text-lg font-bold hover:text-gray-400 transition duration-200">
+          By Eric Formiga &copy; 2024
+        </h1>
+      </div>
+    </footer>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom"
+import AkatsukiCloud from '../../public/AkatsukiCloud.png'
 
 const Navbar = () => {
   return (
-    <nav>
-        <Link to="/">Ir Para a Home</Link>
+    <nav className="bg-gray-800 p-4 flex justify-around shadow-md">
+      <div className="flex items-center gap-16" >
+        <Link to="/">          
+          <img src={AkatsukiCloud} alt="Akatsuki Cloud" className="h-8 w-auto mr-4" />     
+        </Link>
+        <Link to="/" className="text-white text-lg font-bold hover:text-gray-400 transition duration-200">
+          Home
+        </Link>
+      </div>
     </nav>
   )
 }
